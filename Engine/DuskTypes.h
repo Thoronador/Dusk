5ef7eccf3df7035c65b11385a62a6b35971fa00a
Thoronador <thoronador@users.sourceforge.net>
2012-07-19 22:49:04 +0000

    Engine: reorganization of directory structure, part II

--- Engine/DuskTypes.h
+++ Engine/DuskTypes.h
@@ -20,15 +20,18 @@
 
 #ifndef DUSKTYPESH
 #define DUSKTYPESH
+
+#include <stdint.h>
+
 // In this file global types are defined
 
 namespace Dusk
 {
   enum DIRECTION{FORWARD,BACKWARD,LEFT,RIGHT,JUMP_UP,DOWN, TURN_LEFT, TURN_RIGHT};
-  typedef unsigned char uint8; //8 bit unsigned integer, 0-255
+
   struct ColourData
   {
-     uint8 red, green, blue;
+     uint8_t red, green, blue;
   };
 }//namespace
 
